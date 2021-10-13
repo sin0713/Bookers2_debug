76c1e18b452dda7687d4c02ab5a193f8da378861
sin0713 <narisin1124@gmail.com>
2021-10-13 05:41:22 +0000

    [Modefied]users_update_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
-class UsersController < ApplicationController 
-  before_action :ensure_correct_user, only: [:update]
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -11,16 +11,17 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
